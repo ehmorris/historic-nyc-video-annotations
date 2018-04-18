9f7d1e6331ff8fca5310d193a1becfc5658537aa
Edwin Morris <edwin@rapgenius.com>
2018-04-17 22:57:18 -0400

    Render layout

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
         rel: 0,
         showinfo: 0,
         loop: 1,
-        controls: 0,
         modestbranding: 1,
         playsinline: 1,
         autoplay: 1
@@ -23,21 +22,58 @@ class App extends Component {
     });
 
     return (
-      <div>
-        <YouTube
-          videoId={'VxWHKaylRNM'}
-          opts={playerOptions}
-        />
-
-        <Map
-          style='mapbox://styles/mapbox/streets-v9'
-          center={[-73.9, 40.71]}
-          zoom={[9.75]}
-          containerStyle={{
-            width: '400px',
-            height: '400px'
+      <div
+        style={{
+          display: 'flex'
+        }}
+      >
+        <div
+          style={{
+            width: '131.45vh',
+            height: '100vh'
           }}
-        />
+        >
+          <YouTube
+            videoId={'VxWHKaylRNM'}
+            opts={playerOptions}
+          />
+        </div>
+
+        <div
+          style={{
+            flex: '1',
+            minWidth: '35%',
+            position: 'relative'
+          }}
+        >
+          <Map
+            style='mapbox://styles/mapbox/streets-v9'
+            center={[-73.9, 40.71]}
+            zoom={[9.75]}
+            containerStyle={{
+              width: '100%',
+              height: '100%'
+            }}
+          />
+
+          <div
+            style={{
+              position: 'absolute',
+              bottom: '3rem',
+              left: '2rem',
+              right: '2rem',
+              background: '#fff',
+              border: '2px solid #000',
+              padding: '1rem'
+            }}
+          >
+            <pre>
+              Preformatted text
+
+              box
+            </pre>
+          </div>
+        </div>
       </div>
     );
   }
